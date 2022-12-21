0d8987010293513ff62d2a9c87010d3af59e0980
Paul Tanchareon <paul.vtan@live.com>
2022-12-21 12:42:16 +0700

    Basic login component.

--- app_frontend/src/app/app.component.ts
+++ app_frontend/src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { ExternalProvider } from '@ethersproject/providers'
+import { ethers } from 'ethers'
+import MatchFactory from '../../../artifacts/contracts/MatchFactory.sol/MatchFactory.json'
+import { environment } from '../environments/environment'
+
+declare global {
+  interface Window {
+    ethereum: ExternalProvider
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'gesture-strike';
+  title = 'gesture-strike'
+
+  account: ethers.providers.JsonRpcSigner | undefined
+  matchContract: ethers.Contract | undefined
+  matchFactoryContract: ethers.Contract | undefined
+
+  constructor() {}
+
+  connectWallet() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum, 'any')
+    provider.send('eth_requestAccounts', []).then(async () => {
+      this.account = provider.getSigner()
+    })
+    this.initializeContracts(environment.matchFactoryContractAddress)
+  }
+
+  private initializeContracts(address: string) {
+    console.log('initializing contract', address)
+    if (!this.account) return
+    this.matchFactoryContract = new ethers.Contract(
+      environment.matchFactoryContractAddress,
+      MatchFactory.abi,
+      this.account
+    )
+  }
+
+  private async getBalance() {
+    if (!this.account) return
+    const balance = parseFloat(
+      ethers.utils.formatEther(await this.account.getBalance())
+    )
+    console.log('balance', balance)
+    return Math.round(balance * 100) / 100
+  }
 }
---
+++ app_frontend/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  matchFactoryContractAddress: '0x3964823a0F3b42533E392e02B40F867A7238902D',
+}
